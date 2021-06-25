4351641cff4e468f6cc143e0fdb14c2d93f81c3d
evanwill <evanpeterw@gmail.com>
2021-06-25 15:41:04 -0700

    some target blanks

--- _includes/js/highlights.js
+++ _includes/js/highlights.js
@@ -28,7 +28,7 @@ function makeGrid(array) {
   var item;
   // cdmid,size,alt,file
   for (i = 0; i < 12; i++) {
-      item = "<div onclick='yearClick(this)' class='grid-item grid-" + array[i].size + "'><div class='highlight'><img class='himg' src='{{ site.objects }}/highlights_new/" + array[i].file +"' alt='" + array[i].alt + "'><a href='https://digital.lib.uidaho.edu/cdm/ref/collection/argonaut/id/" + array[i].cdmid + "' target='_blank' class='reveal' title='view item'>" + array[i].date + "</a></div></div>";
+      item = "<div onclick='yearClick(this)' class='grid-item grid-" + array[i].size + "'><div class='highlight'><img class='himg' src='{{ site.objects }}/highlights_new/" + array[i].file +"' alt='" + array[i].alt + "'><a href='https://digital.lib.uidaho.edu/cdm/ref/collection/argonaut/id/" + array[i].cdmid + "' target='_blank' rel='noopener' class='reveal' title='view item'>" + array[i].date + "</a></div></div>";
       grid.innerHTML += item;
   }
   /* initialize Packery, metafizzy, http://packery.metafizzy.co/ */
